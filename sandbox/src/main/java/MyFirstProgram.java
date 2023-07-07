2004f2c45eaa16ec6230015aee50b6d1483184d9
Artem_Yurchenko <artem225yurchenko@gmail.com>
2023-07-07 16:22:30 -0400

    Добавлено несколько функций

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,27 @@
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    hello("world");
+    hello("user");
+
+    double l = 5;
+    System.out.println("Площадь кв со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прям со сторонами " + a + " и " + b + " равна " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
